f900ba07940985c205ae84d743ee100ce1a80eb3
yitong86 <sophiajiang86@hotmail.com>
2022-03-22 17:48:17 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/gorestv1/Gorestv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gorestv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gorestv1Application.class, args);
+	}
+
+}
