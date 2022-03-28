83ef8f01e78b57d0935ba00ee9584262ade05133
yitong86 <sophiajiang86@hotmail.com>
2022-03-28 17:39:14 -0400

    post

--- src/main/java/com/careerdevs/gorestv1/controllers/UserController.java
+++ src/main/java/com/careerdevs/gorestv1/controllers/UserController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -20,6 +24,40 @@ public class UserController {
     @GetMapping("/token")
     public String getToken() {
         return env.getProperty("GOREST_TOKEN");
+    }
+
+    ////URL/endpoint http://localhost:4444/api/user/firstpage
+    @GetMapping("/page/{pageNum}")
+    public Object getPage(
+            RestTemplate restTemplate,
+            @PathVariable("pageNum") String pageNumber
+
+    ) {
+        try {
+            String url = "https://gorest.co.in/public/v2/users?page=" + pageNumber;
+
+            ResponseEntity<UserModel[]> response = restTemplate.getForEntity(url, UserModel[].class);
+            UserModel[] firstPageUsers = response.getBody();
+
+            HttpHeaders responseHeaders = response.getHeaders();
+
+            String totalPages = Objects.requireNonNull(responseHeaders.get("X-Pagination-pages")).get(0);
+
+            System.out.println("Total pages: " + totalPages);
+
+            for(int i = 0; i<firstPageUsers.length;i++){
+                UserModel tempUser = firstPageUsers[i];
+                System.out.println(tempUser.toString());
+            }
+
+            return new ResponseEntity<>(firstPageUsers,HttpStatus.OK);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     //URL/endpoint http://localhost:4444/api/user/{id}
@@ -81,13 +119,10 @@ public class UserController {
             return exception.getMessage();
         }
     }
-
-    @PostMapping("/")
+//create
+    @PostMapping("/qp")
     public Object postUserQueryParam(
-            @RequestParam("name") String name,
-            @RequestParam("email") String email,
-            @RequestParam("gender") String gender,
-            @RequestParam("status") String status,
+           @RequestBody UserModel user,
             RestTemplate restTemplate
     ) {
         try {
@@ -96,14 +131,14 @@ public class UserController {
             String token = env.getProperty("GOREST_TOKEN");
             url += "?access-token=" + token;
 
-            UserModel newUser = new UserModel(name, email, gender, status);
+           // UserModel newUser = new UserModel(name, email, gender, status);
 
             //TODO: validate that gender and status are valid before continuing
             //TODO: validate that the email is a valid email
 
-            System.out.println("Date to be sent:\n" + newUser);
+           // System.out.println("Date to be sent:\n" + newUser);
 
-            HttpEntity<UserModel> request = new HttpEntity<>(newUser);
+            HttpEntity<UserModel> request = new HttpEntity<>(user);
 
             return restTemplate.postForEntity(url, request, UserModel.class);
 
@@ -134,5 +169,72 @@ public class UserController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/all")
+    public ResponseEntity getAllUser(RestTemplate restTemplate
+
+    ) {
+
+        try {
+            ArrayList<UserModel> allusers = new ArrayList<>();
+            String url = "https://gorest.co.in/public/v2/users";
+
+            ResponseEntity<UserModel[]> response = restTemplate.getForEntity(url, UserModel[].class);
+
+            allusers.addAll(Arrays.asList(response.getBody()));
+
+            int totalPageNumber = 4;// Integer.parseInt(response.getHeaders().get("X-Pagination-pages").get(0))
+;
+            for(int i = 2;i <= totalPageNumber;i++){
+                String tempUrl = url + "?page=" + i;
+                UserModel[] pageData = restTemplate.getForObject(tempUrl,UserModel[].class);
+                //allusers.addAll(Arrays.asList(pageData));
+                allusers.addAll(Arrays.asList(Objects.requireNonNull(pageData)));
+            }
+            return new ResponseEntity<>(allusers,HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+
+        @PutMapping("/{id}")
+        public ResponseEntity putUser(
+                @PathVariable("id") String userId,
+                RestTemplate restTemplate,
+                @RequestBody UserModel updateData
+
+        ){
+        try{
+            String url = "https://gorest.co.in/public/v2/users/" + userId;
+            String token = env.getProperty("GOREST_TOKEN");
+            url += "?access-token=" + token;
+            HttpEntity<UserModel> request = new HttpEntity<>(updateData);
+            ResponseEntity<UserModel> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.PUT,
+                    request,
+                    UserModel.class
+            );
+            return new ResponseEntity<>(response.getBody(),HttpStatus.OK);
+        }catch(HttpClientErrorException.UnprocessableEntity e){
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        catch(Exception e){
+            System.out.println(e.getClass() + "\n" + e.getMessage());
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+        }
+
+
+
 }
+
 
---
+++ src/main/java/com/careerdevs/gorestv1/model/UserModelArray.java
@@ -0,0 +1,10 @@
+package com.careerdevs.gorestv1.model;
+
+public class UserModelArray {
+
+
+    private UserModel[] users;
+    public  UserModel[] getUsers(){
+        return users;
+    }
+}
